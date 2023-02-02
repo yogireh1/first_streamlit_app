bab52e5c3c1abd7653d564474771c43904fafd9a
yogireh1 <124147073+yogireh1@users.noreply.github.com>
2023-02-02 19:30:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
-def get_frutiyvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
 	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 	return fruityvice_normalized
